99912df541f5110075a1c050bd6dc3d3757f8080
lausuarez88@hotmail.com <lausuarez88@hotmail.com>
2023-06-23 13:48:06 -0300

    agregue modificaciones al JS con funciones

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let numero = 50; 
+function mostrar () {
 
+const numero = parseInt(prompt ("Ingrese un numero: ")); 
 if (numero <= 50) {
     console.log ("El numero es menor que 50");
 }
@@ -11,7 +12,9 @@ else if (numero >30)
 
 else (numero >= 40)
     console.log ("El numero es menor que 40");
+}
 
+mostrar ()
 
 let nombre = prompt ("Ingresar nombre: ");
 
@@ -20,6 +23,7 @@ while (nombre != "ESC") {
     nombre = prompt ("Ingresar otro nombre: ");
 }
 
+function calcular () {
 
 let cantidad = parseFloat(prompt ("Ingresar cantidad"))
 let precio = parseFloat(prompt ("Ingresar precio"))
@@ -27,7 +31,11 @@ let precio = parseFloat(prompt ("Ingresar precio"))
 compra = precio * cantidad;
 desc = compra * 0.15;
 pagar = compra - desc;
-console.log ("El descuento es: + desc")
-console.log ("El total a pagar es: + pagar")
+console.log ("El descuento es: " + desc)
+console.log ("El total a pagar es: " + pagar)
+}
+
+calcular ()
+
 
 
